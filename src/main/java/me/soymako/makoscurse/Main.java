54e33d88fb2ea8534642b6e35e54e3a1a6330a60
SoyMako <correofacilparacosasfaciles@gmail.com>
2025-04-10 08:46:06 -0600

    added files

---
+++ src/main/java/me/soymako/makoscurse/Main.java
@@ -0,0 +1,17 @@
+package me.soymako.makoscurse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
